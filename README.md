7b5fc3f53c1a2c04b427664d13db753a2cff210e
Peyton <66343323+Peyton-C@users.noreply.github.com>
2025-09-26 22:10:47 -0230

    Add Thunderbolt / USB 4 support, include json output examples and fixup readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # lsusb-macos
 WIP Equivalent for lsusb on macOS
+
+A basic python script that converts the output from `system_profiler SPUSBHostDataType` (26.0+) or `system_profiler SPUSBDataType`(>15.0) to be similar to lsusb.
+
+## Sample Output
+`Location: 00100000: ID 154b:1006 PNY USB 3.2.1 FD
+Location: 01100000: ID 303a:1001 Espressif USB JTAG/serial debug unit`
--- lsusb.py
+++ lsusb.py
@@ -36,10 +36,46 @@ def tahoe_usb_fmt():
 
     return lines
 
+def tbolt_usb4_fmt():
+    result = subprocess.run(
+        ["system_profiler", "SPThunderboltDataType", "-json"],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    data = json.loads(result.stdout)
+    lines = []
+
+    for hub in data.get("SPThunderboltDataType", []):
+        for dev in hub.get("_items", []) or []:
+            # Note: These arent a direct eqivilant to normal USB VID and PIDs
+            vid = clean_hex(dev.get("vendor_id_key") or "-")
+            pid = clean_hex(dev.get("device_id_key") or "-")
+            mfr = dev.get("vendor_name_key") or "-"
+            name = dev.get("_name") or "-"
+
+            # Collapse whitespace in name/manufacturer so it's clean on one line
+            mfr = " ".join(str(mfr).split())
+            name = " ".join(str(name).split())
+
+            lines.append(f"ID: {vid}:{pid} {mfr} {name}")
+
+    return lines
+
+
 if platform.system() == "Darwin":
     if float(platform.mac_ver()[0]) >= 26.0:
-        for line in tahoe_usb_fmt():
-            print(line)
+        usb = tahoe_usb_fmt()
+        tb = tbolt_usb4_fmt()
+
+        if usb != []:
+            print("USB Devies:")
+            for line in usb:
+                print(line)
+        if tb != []:
+            print("\nThunderbolt / USB 4 Devices:")
+            for line in tb:
+                print(line)
     else:
         print("Unsupported Version")        
 else:
