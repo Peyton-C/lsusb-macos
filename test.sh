be57ca121706d2cbdbba359bbe845162a3669e4a
Peyton <66343323+Peyton-C@users.noreply.github.com>
2025-09-29 13:40:29 -0230

    Minor testing script formatting changes

--- test.sh
+++ test.sh
@@ -2,17 +2,17 @@
 # Automatically try every supported combination
 
 # Tahoe and Newer
-printf "Tahoe"
+printf "Tahoe\n"
 python3 ./lsusb.py -D USB ./json_examples/tahoe_usb_full.json -D TB ./json_examples/tahoe_tb.json -os 26.1
 
 # Sequoia - Catalina
-printf "\nSequoia - USB\n"
+printf "\n\nSequoia\n"
 python3 ./lsusb.py -D USB ./json_examples/sequoia_intel_usb_full.json -D TB ./json_examples/sequoia_intel_tb.json -os  15.5
 
 # Mojave - Yosemite
-printf "\nMojave - USB\n" 
+printf "\n\nMojave\n" 
 python3 ./lsusb.py -D USB ./json_examples/hs_xml_usb_basic.plist -os  10.13 
 
 # Mavricks and Older
-printf "\nMavricks - USB\n"
+printf "\n\nMavricks\n"
 python3 ./lsusb.py -D USB ./json_examples/lion_xml_usb_basic.plist -os 10.7
